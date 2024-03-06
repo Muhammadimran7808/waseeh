eb437fd2e86372d871a2cd01e63d6823cc7ad69e
Muhammad Imran <imran.sahadvi@gmail.com>
2024-03-06 16:06:50 +0500

    intilizing project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
